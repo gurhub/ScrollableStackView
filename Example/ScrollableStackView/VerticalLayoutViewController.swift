bb5a07c48fa6ef271d3e3b6e3fd513c53d263791
Simon Strandgaard <neoneye@gmail.com>
2017-10-27 10:34:24 +0100

    removed unused code

--- Example/ScrollableStackView/VerticalLayoutViewController.swift
+++ Example/ScrollableStackView/VerticalLayoutViewController.swift
@@ -57,9 +57,4 @@ class VerticalLayoutViewController: UIViewController {
         
         super.updateViewConstraints()
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 }
